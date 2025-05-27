e28a93f606e2d61cfdce5d3b064b21294c4d0c74
hiraishi yuuta <hiraishi.yuuta.td@gmail.com>
2025-05-27 14:27:08 +0900

    add fetch api web

--- Web/app/routes/home.tsx
+++ Web/app/routes/home.tsx
@@ -1,4 +1,6 @@
+import { client } from "openapi/client";
 import type { Route } from "./+types/home";
+import { Form } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +9,51 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <div>Hello World</div>;
+export async function loader(){
+  const { data } = await client.GET("/inventories", {})
+
+  return data
+}
+
+export async function action({
+  request
+}: Route.ActionArgs){
+  // let formData = await request.formData();
+  switch (request.method){
+    case 'POST': {
+      await client.POST("/inventories", {
+        body: {
+          itemMaster: {
+            itemCode: "test",
+            itemName: "test",
+            classification: {
+              item: "test",
+              asset: "test"
+            },
+          },
+          lotNo: "testlot",
+          qty: 100
+        }
+      })
+    }
+  }
+}
+
+export default function Home({
+  loaderData
+}: Route.ComponentProps) { 
+
+  return (<div>
+    <div>Home</div>
+    {loaderData?.map(d => (
+      <div className="flex">
+        <div>{d.lotNo}</div>
+        <div>{d.itemMaster?.itemName}</div>
+        <div>{d.qty}</div> 
+      </div>
+    ))}
+    <Form method="POST">
+      <button type="submit">Save</button>
+    </Form>
+    </div>)
 }
